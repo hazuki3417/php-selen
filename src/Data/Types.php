d73266cbe55f4bc62bec6a4812856cf8110165ee
Takashi Kudo <hazuki3417@gmail.com>
2022-08-05 18:44:39 +0900

    chore: add types class

---
+++ src/Data/Types.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @license MIT
+ * @author hazuki3417<hazuki3417@gmail.com>
+ * @copyright 2021 hazuki3417 all rights reserved.
+ */
+
+namespace Selen\Data;
+
+class Types
+{
+    public static function validate($data, string ...$typeNames): bool
+    {
+        $isDuplicateTypeName = count(\array_unique($typeNames)) < count($typeNames);
+        if($isDuplicateTypeName){
+            throw new \InvalidArgumentException('Duplicate type specification.');
+        }
+
+        $results = [];
+        foreach ($typeNames as $typeName) {
+            $results[] = Type::validate($data, $typeName);
+        }
+        return \in_array(true, $results, true);
+    }
+}
---
+++ tests/Data/TypesTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * @license MIT
+ * @author hazuki3417<hazuki3417@gmail.com>
+ * @copyright 2021 hazuki3417 all rights reserved.
+ */
+
+namespace Selen\Data\Structure\Test;
+
+use PHPUnit\Framework\TestCase;
+use Selen\Data\Types;
+
+/**
+ * @coversDefaultClass \Selen\Data\Types
+ *
+ * @group Selen/Data
+ * @group Selen/Data/Types
+ *
+ * @see \Selen\Data\Types
+ *
+ * [command]
+ * php ./vendor/bin/phpunit --group=Selen/Data/Types
+ *
+ * @internal
+ */
+class TypesTest extends TestCase
+{
+    public function dataProviderValidate()
+    {
+        return [
+            'pattern001' => ['expected' => true,  'input' => ['typeName1' => 'int',    'typeName2' => 'string', 'data' => 0]],
+            'pattern002' => ['expected' => true,  'input' => ['typeName1' => 'int',    'typeName2' => 'string', 'data' => '0']],
+            'pattern003' => ['expected' => false, 'input' => ['typeName1' => 'string', 'typeName2' => 'array',  'data' => true]],
+        ];
+    }
+
+    /**
+     * @dataProvider dataProviderValidate
+     *
+     * @param mixed $expected
+     * @param mixed $input
+     */
+    public function testValidate($expected, $input)
+    {
+        $this->assertEquals(
+            $expected,
+            Types::validate($input['data'], $input['typeName1'], $input['typeName2'])
+        );
+    }
+
+    public function testValidateException()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+
+        Types::validate("inputValue", "string", "string");
+    }
+}
