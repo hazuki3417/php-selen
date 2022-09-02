6d6e047a9acfdf27248e1ca5f9149707d9e96c65
Takashi Kudo <hazuki3417@gmail.com>
2022-09-02 09:36:03 +0900

    chore: add phpdocs

--- src/Str/CaseName.php
+++ src/Str/CaseName.php
@@ -13,6 +13,9 @@ namespace Selen\Str;
  */
 class CaseName
 {
+    /**
+     * ケバブケースに変換します
+     */
     public static function kebab(string $value): string
     {
         $delimiter = '-';
@@ -20,6 +23,9 @@ class CaseName
         return self::lower(self::replaceDelimiter($delimiter, $tmp));
     }
 
+    /**
+     * スネークケースに変換します
+     */
     public static function snake(string $value): string
     {
         $delimiter = '_';
@@ -27,6 +33,9 @@ class CaseName
         return self::lower(self::replaceDelimiter($delimiter, $tmp));
     }
 
+    /**
+     * pascalケースに変換します
+     */
     public static function pascal(string $value): string
     {
         $delimiter = ' ';
@@ -35,6 +44,9 @@ class CaseName
         return \ucfirst(\preg_replace('/[ ]+/', '', $str));
     }
 
+    /**
+     * キャメルケースに変換します
+     */
     public static function camel(string $value): string
     {
         $delimiter = ' ';
@@ -43,28 +55,40 @@ class CaseName
         return \lcfirst(\preg_replace('/[ ]+/', '', $str));
     }
 
+    /**
+     * ローワーケースに変換します
+     */
     public static function lower(string $value): string
     {
         return \mb_strtolower($value);
     }
 
+    /**
+     * アッパーケースに変換します
+     */
     public static function upper(string $value): string
     {
         return \mb_strtoupper($value);
     }
 
+    /**
+     * 文字列の前後にある特定の文字（' ', '_', '-'）をトリムします
+     */
     private static function trim(string $value): string
     {
         return trim($value, ' _-');
     }
 
+    /**
+     * 文字列内にある特定の文字（' ', '_', '-'）を置換します
+     */
     private static function replaceDelimiter(string $delimiter, string $value): string
     {
         return \preg_replace('/[-|_| ]+/', $delimiter, $value);
     }
 
     /**
-     * キャメルケースに該当する文字の先頭に区切り文字（half space）を追加します
+     * キャメルケースに該当する文字の先頭に区切り文字（' '）を追加します
      */
     private static function addCamelCaseDelimiter(string $value): string
     {
