77eb2017a4495869ccea7d75d7b1c24d1d7fd09b
Takashi Kudo <hazuki3417@gmail.com>
2023-01-05 10:37:49 +0900

    chore: fix test code

--- tests/Schema/ExchangerTest.php
+++ tests/Schema/ExchangerTest.php
@@ -56,12 +56,12 @@ class ExchangerTest extends TestCase
                     'value1 + add string',
                     'value2 + add string',
                 ],
-                'input' => [
-                    'value' => [
+                'input'    => [
+                    'value'                => [
                         'value1',
                         'value2',
                     ],
-                    'define' => new ArrayDefine(
+                    'define'               => new ArrayDefine(
                         // keyを指定しない場合、callableの引数には配列内の要素値が渡されます
                         Define::noKey()->value(function ($value) {
                             return $value . ' + add string';
@@ -77,12 +77,12 @@ class ExchangerTest extends TestCase
                     'value1 + add string 0',
                     'value2 + add string 1',
                 ],
-                'input' => [
-                    'value' => [
+                'input'    => [
+                    'value'                => [
                         'value1',
                         'value2',
                     ],
-                    'define' => new ArrayDefine(
+                    'define'               => new ArrayDefine(
                         // keyを指定した場合、callableの引数にはkey名に対応する値が渡されます
                         // key名にindexを指定した場合は配列が持っているindexを参照します
                         // int型の0とstring型の'0'は同じindexとして扱います（php配列の仕様）
@@ -103,12 +103,12 @@ class ExchangerTest extends TestCase
                     3         => 'value1 + add string index key = 3',
                     'keyName' => 'value2 + add string index key = keyName',
                 ],
-                'input' => [
-                    'value' => [
+                'input'    => [
+                    'value'                => [
                         3         => 'value1',
                         'keyName' => 'value2',
                     ],
-                    'define' => new ArrayDefine(
+                    'define'               => new ArrayDefine(
                         Define::key(3)->value(function ($value) {
                             return $value .= ' + add string index key = 3';
                         }),
@@ -137,8 +137,8 @@ class ExchangerTest extends TestCase
                         ],
                     ],
                 ],
-                'input' => [
-                    'value' => [
+                'input'    => [
+                    'value'                => [
                         'keyName1'       => 'value1',
                         'parentKeyName1' => [
                             'childKeyName1-1' => 'childKeyValue1-1',
@@ -153,7 +153,7 @@ class ExchangerTest extends TestCase
                             ],
                         ],
                     ],
-                    'define' => new ArrayDefine(
+                    'define'               => new ArrayDefine(
                         Define::key('parentKeyName1')->arrayDefine(
                             Define::key('childKeyName1-2')->value($valueExchangeStub1)
                         ),
@@ -176,17 +176,17 @@ class ExchangerTest extends TestCase
                         'childKeyName1-2' => 'childKeyValue1-2',
                         'childKeyName1-1' => null,
                     ],
-                    'keyName3' => null,
+                    'keyName3'       => null,
                 ],
-                'input' => [
-                    'value' => [
+                'input'    => [
+                    'value'                => [
                         'keyName1'       => 'value1',
                         'keyName2'       => 'value2',
                         'parentKeyName1' => [
                             'childKeyName1-2' => 'childKeyValue1-2',
                         ],
                     ],
-                    'define' => new ArrayDefine(
+                    'define'               => new ArrayDefine(
                         Define::key('keyName2', Define::KEY_ACTION_ADD),
                         Define::key('keyName3', Define::KEY_ACTION_ADD),
                         Define::key('parentKeyName1')->arrayDefine(
@@ -205,8 +205,8 @@ class ExchangerTest extends TestCase
                         'childKeyName1-2' => 'childKeyValue1-2',
                     ],
                 ],
-                'input' => [
-                    'value' => [
+                'input'    => [
+                    'value'                => [
                         'keyName1'       => 'value1',
                         'keyName2'       => 'value2',
                         'parentKeyName1' => [
@@ -214,7 +214,7 @@ class ExchangerTest extends TestCase
                             'childKeyName1-2' => 'childKeyValue1-2',
                         ],
                     ],
-                    'define' => new ArrayDefine(
+                    'define'               => new ArrayDefine(
                         Define::key('keyName2', Define::KEY_ACTION_REMOVE),
                         Define::key('parentKeyName1')->arrayDefine(
                             Define::key('childKeyName1-1', Define::KEY_ACTION_REMOVE)
@@ -231,16 +231,16 @@ class ExchangerTest extends TestCase
                     'parentKeyName1' => [
                         'renameChildKey1-1' => 'childKeyValue1-1',
                     ],
-                    'renameKey1' => 'value1',
+                    'renameKey1'     => 'value1',
                 ],
-                'input' => [
-                    'value' => [
+                'input'    => [
+                    'value'                => [
                         'keyName1'       => 'value1',
                         'parentKeyName1' => [
                             'childKeyName1-1' => 'childKeyValue1-1',
                         ],
                     ],
-                    'define' => new ArrayDefine(
+                    'define'               => new ArrayDefine(
                         Define::key('keyName1', Define::KEY_ACTION_RENAME, function ($keyName) {
                             return 'renameKey1';
                         }),
@@ -262,10 +262,10 @@ class ExchangerTest extends TestCase
                         'childKeyName1-2'   => 'childKeyValue1-2',
                         'renameChildKey1-1' => 'replaceValue1-1',
                     ],
-                    'renameKey1' => 'replaceValue1',
+                    'renameKey1'     => 'replaceValue1',
                 ],
-                'input' => [
-                    'value' => [
+                'input'    => [
+                    'value'                => [
                         'keyName1'       => 'value1',
                         'keyName2'       => 'value2',
                         'parentKeyName1' => [
@@ -273,7 +273,7 @@ class ExchangerTest extends TestCase
                             'childKeyName1-2' => 'childKeyValue1-2',
                         ],
                     ],
-                    'define' => new ArrayDefine(
+                    'define'               => new ArrayDefine(
                         Define::key('keyName1', Define::KEY_ACTION_RENAME, $keyExchangeStub1)->value(function ($value) {
                             return 'replaceValue1';
                         }),
@@ -297,8 +297,8 @@ class ExchangerTest extends TestCase
                         'child_key_name1_2' => 'childKeyValue1-2',
                     ],
                 ],
-                'input' => [
-                    'value' => [
+                'input'    => [
+                    'value'                => [
                         'keyName1'       => 'value1',
                         'keyName2'       => 'value2',
                         'parentKeyName1' => [
@@ -306,8 +306,8 @@ class ExchangerTest extends TestCase
                             'childKeyName1-2' => 'childKeyValue1-2',
                         ],
                     ],
-                    'define'             => null,
-                    'keyExchangeExecute' => function ($key) {
+                    'define'               => null,
+                    'keyExchangeExecute'   => function ($key) {
                         return CaseName::snake($key);
                     },
                     'valueExchangeExecute' => null,
@@ -323,8 +323,8 @@ class ExchangerTest extends TestCase
                         'childKeyName1-2' => 'child_key_value1_2',
                     ],
                 ],
-                'input' => [
-                    'value' => [
+                'input'    => [
+                    'value'                => [
                         'keyName1'       => 'value1',
                         'keyName2'       => 'value2',
                         'parentKeyName1' => [
@@ -364,8 +364,8 @@ class ExchangerTest extends TestCase
                     'queue_status' => 'success add string',
                     'use_state'    => false,
                 ],
-                'input' => [
-                    'value' => [
+                'input'    => [
+                    'value'                => [
                         'id'         => '630f1d9dc7636af90c0d73f6',
                         'dummyKey1'  => 'dummyValue1',
                         'dummyKey2'  => 'dummyValue2',
@@ -377,7 +377,7 @@ class ExchangerTest extends TestCase
                         'createdAt'  => new DateTime('2022-09-05'),
                         'updatedAt'  => new DateTime('2022-09-12'),
                     ],
-                    'define' => new ArrayDefine(
+                    'define'               => new ArrayDefine(
                         // key削除
                         Define::key('id', Define::KEY_ACTION_REMOVE),
                         // keyリネーム
@@ -397,7 +397,7 @@ class ExchangerTest extends TestCase
                             return $value->getTimestamp();
                         })
                     ),
-                    'keyExchangeExecute' => function ($key) {
+                    'keyExchangeExecute'   => function ($key) {
                         return CaseName::snake($key);
                     },
                     'valueExchangeExecute' => function ($value) {
@@ -427,11 +427,11 @@ class ExchangerTest extends TestCase
                             'user-640f1d9dc7636af90c0d73f9',
                             'user-640f1d9dc7636af90c0d7400',
                         ],
-                        'uuid' => 'user-650f1d9dc7636af90c0d73f6',
+                        'uuid'    => 'user-650f1d9dc7636af90c0d73f6',
                     ],
                 ],
-                'input' => [
-                    'value' => [
+                'input'    => [
+                    'value'                => [
                         'loginId' => '620f1d9dc7636af90c0d73f6',
                         'linkIds' => [
                             '630f1d9dc7636af90c0d73f6',
@@ -448,11 +448,11 @@ class ExchangerTest extends TestCase
                                 '640f1d9dc7636af90c0d73f9',
                                 '640f1d9dc7636af90c0d7400',
                             ],
-                            'uuid' => '650f1d9dc7636af90c0d73f6',
+                            'uuid'    => '650f1d9dc7636af90c0d73f6',
                         ],
                     ],
-                    'define'             => new ArrayDefine(),
-                    'keyExchangeExecute' => function ($key) {
+                    'define'               => new ArrayDefine(),
+                    'keyExchangeExecute'   => function ($key) {
                         // keyの変換はテストしないのでそのまま返す
                         return $key;
                     },
@@ -476,8 +476,8 @@ class ExchangerTest extends TestCase
                         'user-630f1d9dc7636af90c0d7400',
                     ],
                 ],
-                'input' => [
-                    'value' => [
+                'input'    => [
+                    'value'                => [
                         'loginId' => '620f1d9dc7636af90c0d73f6',
                         'linkIds' => [
                             '630f1d9dc7636af90c0d73f6',
@@ -487,7 +487,7 @@ class ExchangerTest extends TestCase
                             '630f1d9dc7636af90c0d7400',
                         ],
                     ],
-                    'define' => new ArrayDefine(
+                    'define'               => new ArrayDefine(
                         Define::key('linkIds')->arrayDefine(
                             Define::noKey()->value(function ($value) {
                                 if (\is_string($value)) {
@@ -498,7 +498,7 @@ class ExchangerTest extends TestCase
                             })
                         )
                     ),
-                    'keyExchangeExecute' => function ($key) {
+                    'keyExchangeExecute'   => function ($key) {
                         // keyの変換はテストしないのでそのまま返す
                         return $key;
                     },
@@ -515,13 +515,13 @@ class ExchangerTest extends TestCase
                     // NOTE: key名を変換するexecuteがなくても（null）順番は変わる
                     'key1' => '620f1d9dc7636af90c0d73f6',
                 ],
-                'input' => [
-                    'value' => [
+                'input'    => [
+                    'value'                => [
                         'key1' => '620f1d9dc7636af90c0d73f6',
                         'key2' => '620f1d9dc7636af90c0d73f6',
                         'key3' => '620f1d9dc7636af90c0d73f6',
                     ],
-                    'define' => new ArrayDefine(
+                    'define'               => new ArrayDefine(
                         // executeにnullを設定した場合、keyの変換処理は行わない
                         Define::key('key1', Define::KEY_ACTION_RENAME, null),
                         // executeにnullを設定した場合、valueの変換処理は行わない
@@ -547,8 +547,8 @@ class ExchangerTest extends TestCase
                             'name' => '名前3',
                         ],                    ],
                 ],
-                'input' => [
-                    'value' => [
+                'input'    => [
+                    'value'                => [
                         'objects' => [
                             [
                                 'id'   => 'ユニークID1',
@@ -564,7 +564,7 @@ class ExchangerTest extends TestCase
                             ],
                         ],
                     ],
-                    'define' => new ArrayDefine(
+                    'define'               => new ArrayDefine(
                         Define::key('objects')->arrayDefine(
                             Define::noKey()->arrayDefine(
                                 Define::key('id')->value(function ($value) {
@@ -572,6 +572,55 @@ class ExchangerTest extends TestCase
                                 })
                             )
                         ),
+                    ),
+                    'keyExchangeExecute'   => null,
+                    'valueExchangeExecute' => null,
+                ],
+            ],
+            'pattern016' => [
+                'expected' => [
+                    'childObject'       => [
+                        '_id'  => 'childObjectId',
+                        'name' => '名前1',
+                    ],
+                    'childArrayObjects' => [
+                        [
+                            '_id'  => 'childArrayObjectId1',
+                            'name' => '名前1',
+                        ],
+                        [
+                            '_id'  => 'childArrayObjectId2',
+                            'name' => '名前2',
+                        ],
+                        [
+                            '_id'  => 'childArrayObjectId3',
+                            'name' => '名前3',
+                        ],                    ],
+                ],
+                'input'    => [
+                    'value'                => [
+                        '_id'          => 'parentId',
+                        'childObject'  => [
+                            '_id'  => 'childObjectId',
+                            'name' => '名前1',
+                        ],
+                        'childArrayObjects' => [
+                            [
+                                '_id'  => 'childArrayObjectId1',
+                                'name' => '名前1',
+                            ],
+                            [
+                                '_id'  => 'childArrayObjectId2',
+                                'name' => '名前2',
+                            ],
+                            [
+                                '_id'  => 'childArrayObjectId3',
+                                'name' => '名前3',
+                            ],
+                        ],
+                    ],
+                    'define'               => new ArrayDefine(
+                        Define::key('_id', Define::KEY_ACTION_REMOVE),
                     ),
                     'keyExchangeExecute'   => null,
                     'valueExchangeExecute' => null,
