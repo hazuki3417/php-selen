6821d376c5f097e3d24f3bbf4805f6b228dbfd34
Takashi Kudo <hazuki3417@gmail.com>
2023-03-17 14:48:59 +0900

    chore: add \Str\Random class

---
+++ src/Str/Random.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * @license MIT
+ * @author hazuki3417<hazuki3417@gmail.com>
+ * @copyright 2021 hazuki3417 all rights reserved.
+ */
+
+namespace Selen\Str;
+
+/**
+ * ランダムな文字列を返すクラス
+ */
+class Random
+{
+    /**
+     * プールからランダムな1文字を返します
+     *
+     * @param string $pool プールする文字列を指定します
+     *
+     * @return string ランダムな文字を返します
+     */
+    public static function char(string $pool): string
+    {
+        $index = mt_rand(0, strlen($pool) - 1);
+        return \substr($pool, $index, 1);
+    }
+
+    /**
+     * プールからランダムな文字列を返します
+     *
+     * @param string $pool プールする文字列を指定します
+     * @param int $length 文字列長を指定します
+     *
+     * @return string ランダムな文字列を返します
+     */
+    public static function str(string $pool, int $length): string
+    {
+        $str = '';
+
+        for ($i = 0; $i < $length; ++$i) {
+            $str .= self::char($pool);
+        }
+        return $str;
+    }
+
+    /**
+     * ランダムなアルファベット文字列を返します
+     *
+     * @param int $length 文字列長を指定します
+     *
+     * @return string ランダムな文字列を返します
+     */
+    public static function alpha(int $length): string
+    {
+        $pool = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        return self::str($pool, $length);
+    }
+
+    /**
+     * ランダムな数字文字列を返します
+     *
+     * @param int $length 文字列長を指定します
+     *
+     * @return string ランダムな文字列を返します
+     */
+    public static function num(int $length): string
+    {
+        $pool = '0123456789';
+        return self::str($pool, $length);
+    }
+
+    /**
+     * ランダムなアルファベット・数字文字列を返します
+     *
+     * @param int $length 文字列長を指定します
+     *
+     * @return string ランダムな文字列を返します
+     */
+    public static function alphaNum(int $length): string
+    {
+        $pool = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        return self::str($pool, $length);
+    }
+}
---
+++ tests/Str/RandomTest.php
@@ -0,0 +1,89 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * @license MIT
+ * @author hazuki3417<hazuki3417@gmail.com>
+ * @copyright 2021 hazuki3417 all rights reserved.
+ */
+
+namespace Tests\Selen\Str;
+
+use PHPUnit\Framework\TestCase;
+use Selen\Str\Random;
+
+/**
+ * @coversDefaultClass \Selen\Str\Random
+ *
+ * @group Selen/Str
+ * @group Selen/Str/Random
+ *
+ * @see \Selen\Str\Random
+ *
+ * [command]
+ * php ./vendor/bin/phpunit --group=Selen/Str/Random
+ *
+ * @internal
+ */
+class RandomTest extends TestCase
+{
+    public function testChar()
+    {
+        $pool      = 'abcde';
+        $poolChars = \str_split($pool);
+
+        $this->assertTrue(\in_array(Random::char($pool), $poolChars, true));
+    }
+
+    public function testStr()
+    {
+        $pool      = 'abcde';
+        $poolChars = \str_split($pool);
+
+        $randomStr   = Random::str($pool, 10);
+        $randomChars = \str_split($randomStr);
+
+        foreach ($randomChars as $randomChar) {
+            $this->assertTrue(\in_array($randomChar, $poolChars, true));
+        }
+    }
+
+    public function testAlpha()
+    {
+        $pool      = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $poolChars = \str_split($pool);
+
+        $randomStr   = Random::alpha(10);
+        $randomChars = \str_split($randomStr);
+
+        foreach ($randomChars as $randomChar) {
+            $this->assertTrue(\in_array($randomChar, $poolChars, true));
+        }
+    }
+
+    public function testNum()
+    {
+        $pool      = '0123456789';
+        $poolChars = \str_split($pool);
+
+        $randomStr   = Random::num(10);
+        $randomChars = \str_split($randomStr);
+
+        foreach ($randomChars as $randomChar) {
+            $this->assertTrue(\in_array($randomChar, $poolChars, true));
+        }
+    }
+
+    public function testAlphaNum()
+    {
+        $pool      = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $poolChars = \str_split($pool);
+
+        $randomStr   = Random::alphaNum(10);
+        $randomChars = \str_split($randomStr);
+
+        foreach ($randomChars as $randomChar) {
+            $this->assertTrue(\in_array($randomChar, $poolChars, true));
+        }
+    }
+}
