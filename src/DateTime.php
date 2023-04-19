4b049f853f1b67d3e5c7f123d68d01be83dbb843
Takashi Kudo <hazuki3417@gmail.com>
2023-04-19 11:23:22 +0900

    chore: 日付比較処理を \DateTime\Util クラスに委譲

--- src/DateTime.php
+++ src/DateTime.php
@@ -22,97 +22,6 @@ class DateTime
     }
 
     /**
-     * 日付が未来かどうか確認します
-     *
-     * @return bool 未来の場合はtrueを、それ以外の場合はfalseを返します
-     */
-    public function isFeature(): bool
-    {
-        return \time() < $this->record->toDateTime()->getTimestamp();
-    }
-
-    /**
-     * 日付が過去かどうか確認します
-     *
-     * @return bool 過去の場合はtrueを、それ以外の場合はfalseを返します
-     */
-    public function isPast(): bool
-    {
-        return $this->record->toDateTime()->getTimestamp() < \time();
-    }
-
-    /**
-     * 自身の日付情報と入力の日付情報が同じか確認します
-     *
-     * @return bool 同じ場合はtrueを、それ以外の場合はfalseを返します
-     */
-    public function eq(\DateTime|DateTime $dateTime): bool
-    {
-        $selfDateTime  = $this->record->toDateTime();
-        $inputDateTime = $this->extractInstance($dateTime);
-        return $selfDateTime->getTimestamp() === $inputDateTime->getTimestamp();
-    }
-
-    /**
-     * 自身の日付情報と入力の日付情報が異なるか確認します
-     *
-     * @return bool 異なる場合はtrueを、それ以外の場合はfalseを返します
-     */
-    public function ne(\DateTime|DateTime $dateTime): bool
-    {
-        $selfDateTime  = $this->record->toDateTime();
-        $inputDateTime = $this->extractInstance($dateTime);
-        return $selfDateTime->getTimestamp() !== $inputDateTime->getTimestamp();
-    }
-
-    /**
-     * 自身の日付が入力日付より大きい確認します（$this > $dateTime）
-     *
-     * @return bool 大きい場合はtrueを、それ以外の場合はfalseを返します
-     */
-    public function gt(\DateTime|DateTime $dateTime): bool
-    {
-        $selfDateTime  = $this->record->toDateTime();
-        $inputDateTime = $this->extractInstance($dateTime);
-        return $selfDateTime->getTimestamp() > $inputDateTime->getTimestamp();
-    }
-
-    /**
-     * 自身の日付が入力日付以上か確認します（$this >= $dateTime）
-     *
-     * @return bool 以上の場合はtrueを、それ以外の場合はfalseを返します
-     */
-    public function ge(\DateTime|DateTime $dateTime): bool
-    {
-        $selfDateTime  = $this->record->toDateTime();
-        $inputDateTime = $this->extractInstance($dateTime);
-        return $selfDateTime->getTimestamp() >= $inputDateTime->getTimestamp();
-    }
-
-    /**
-     * 自身の日付が入力日付以下か確認します（$this <= $dateTime）
-     *
-     * @return bool 以下の場合はtrueを、それ以外の場合はfalseを返します
-     */
-    public function le(\DateTime|DateTime $dateTime): bool
-    {
-        $selfDateTime  = $this->record->toDateTime();
-        $inputDateTime = $this->extractInstance($dateTime);
-        return $selfDateTime->getTimestamp() <= $inputDateTime->getTimestamp();
-    }
-
-    /**
-     * 自身の日付が入力日付より小さいか確認します（$this < $dateTime）
-     *
-     * @return bool 小さい場合はtrueを、それ以外の場合はfalseを返します
-     */
-    public function lt(\DateTime|DateTime $dateTime): bool
-    {
-        $selfDateTime  = $this->record->toDateTime();
-        $inputDateTime = $this->extractInstance($dateTime);
-        return $selfDateTime->getTimestamp() < $inputDateTime->getTimestamp();
-    }
-    /**
      * 日付文字列からインスタンスを生成します
      *
      * @param string $parseFormat パース文字列
@@ -137,13 +46,5 @@ class DateTime
         $format   = 'Y-m-d H:i:s';
         $dateTime = \date($format, $timestamp);
         return new DateTime(Record::parseStr($format, $dateTime));
-    }
-
-    private function extractInstance(\DateTime|DateTime $dateTime): \DateTime
-    {
-        if ($dateTime instanceof DateTime) {
-            return $dateTime->record->toDateTime();
-        }
-        return $dateTime;
     }
 }
