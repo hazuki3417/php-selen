b646e16e20a9be2f38b607f765a63bae8d003058
Takashi Kudo <hazuki3417@gmail.com>
2023-04-19 14:29:09 +0900

    chore: fix namespace

--- tests/Bool/UtilTest.php
+++ tests/Bool/UtilTest.php
@@ -7,7 +7,7 @@ declare(strict_types=1);
  * @copyright 2021 hazuki3417 all rights reserved.
  */
 
-namespace Tests\Selen\Str;
+namespace Tests\Selen\Bool;
 
 use PHPUnit\Framework\TestCase;
 use Selen\Bool\Util;
